73deb520b400fbcb7d9fa235cc0404fec485d378
Ruben Cedeno <rubencedeno@Rubens-MBP.verizon.net>
2021-01-07 09:16:40 -0500

    [readme] Add line for get ready for review

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 8000;
 const users = require('./api/users');
 
 //Middleware
+
+
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
